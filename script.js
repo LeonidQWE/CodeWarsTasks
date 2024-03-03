41acacc4cec69a9cabbce987a533818cafc45eae
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2024-03-03 20:13:03 +0300

    add task

--- script.js
+++ script.js
@@ -556,8 +556,31 @@
 // console.log(rounders(10)) //10
 
 
+//-----------------------------------------------------
+//-----------------------6kyu--------------------------
+//-----------------------------------------------------
+// In this kata, you will sort elements in an array by decreasing frequency of elements. If two elements have the same frequency, sort them by increasing value
+// function solve(arr){
+//   const objOfNumber = {};
 
+//   arr.forEach((item) => {
+//     objOfNumber[item] = (objOfNumber[item] || 0) + 1;
+//   })
 
+//   arr.sort((a, b) => {
+//     if (objOfNumber[a] === objOfNumber[b]) {
+//       return a - b;
+//     }
+//     return objOfNumber[b] - objOfNumber[a];
+//   });
 
+//   return arr;
+// }
+
+// console.log(solve([2,3,5,3,7,9,5,3,7])) //[3,3,3,5,5,7,7,2,9]
+// console.log(solve([1,2,3,0,5,0,1,6,8,8,6,9,1])) //[1,1,1,0,0,6,6,8,8,2,3,5,9]
+// console.log(solve([5,9,6,9,6,5,9,9,4,4])) //[9,9,9,9,4,4,5,5,6,6]
+// console.log(solve([4,4,2,5,1,1,3,3,2,8])) //[1,1,2,2,3,3,4,4,5,8]
+// console.log(solve([4,9,5,0,7,3,8,4,9,0])) //[0,0,4,4,9,9,3,5,7,8]
 
 
