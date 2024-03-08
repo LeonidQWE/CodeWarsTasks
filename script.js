9da5adfbc4ff93a02c287ee08fa040af6027aee5
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2024-03-08 11:23:57 +0300

    add tasks

--- script.js
+++ script.js
@@ -611,7 +611,71 @@
 // ]
 // console.log(groupAnagrams(["rat", "tar", "star"])) //[["rat", "tar"], ["star"]]
 
+//---------------------------------------------------------------------------
 
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+// function findUniq(arr) {
+//   const objOfArr = {};
 
+//   arr.forEach((item, index) => {
+//     if (objOfArr[item]) {
+//       objOfArr[item] += 1;
+//     } else {
+//       objOfArr[item] = 1;
+//     }
+//   })
 
+//   for (let key in objOfArr) {
+//     if (objOfArr[key] === 1) {
+//       return Number(key)
+//     }
+//   }
+// }
+
+// // clever
+// // function findUniq (arr) {
+// //   arr.sort();
+
+// //   if (arr[0] === arr[1]) {
+// //     return arr[arr.length - 1]
+// //   } else {
+// //     return arr[0]
+// //   }
+// // }
+
+// console.log(findUniq([ 1, 0, 0 ])) //1
+// console.log(findUniq([ 0, 1, 0 ])) //1
+// console.log(findUniq([ 0, 0, 1 ])) //1
+// console.log(findUniq([ 1, 1, 1, 2, 1, 1 ])) //2
+// console.log(findUniq([ 1, 1, 2, 1, 1 ])) //2
+// console.log(findUniq([ 3, 10, 3, 3, 3 ])) //10
+
+//---------------------------------------------------------------------------
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+// Additionally, if the number is negative, return 0.
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+// Courtesy of projecteuler.net (Problem 1)
+// function solution(number){
+//   let sum = 0;
+
+//   if (number <= 0) {
+//     return 0;
+//   }
+
+//   for (let i = 1; i < number; i++) {
+//     if (i % 3 === 0 || i % 5 === 0) {
+//       sum += i
+//     }
+//   }
+
+//   return sum
+// }
+
+// console.log(solution(3)) // 3
+// console.log(solution(5)) // 8
+// console.log(solution(10)) // 23
+// console.log(solution(15)) // 45
+// console.log(solution(20)) // 78
 
