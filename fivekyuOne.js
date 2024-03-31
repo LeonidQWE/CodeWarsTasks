a452540e26dd51fca7901baea07cb11f32bc7b1f
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2024-03-31 20:11:29 +0300

    add task

---
+++ fivekyuOne.js
@@ -0,0 +1,19 @@
+//-----------------------5kyu (part 1)--------------------------
+//--------------------------------------------------------------
+
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+// function moveZeros(arr) {
+//   const arrClone = structuredClone(arr);
+//   return arrClone.sort((a, b) => {
+//     if (b === 0) {
+//       return -1;
+//     }
+//   })
+// }
+
+// console.log(moveZeros([1,2,0,1,0,1,0,3,0,1])) //[1, 2, 1, 1, 3, 1, 0, 0, 0, 0]
+// console.log(moveZeros([false,1,0,1,2,0,1,3,"a"])) //[false, 1, 1, 2, 1, 3, 'a', 0, 0]
+
+//--------------------------------------------------------------
+
+
