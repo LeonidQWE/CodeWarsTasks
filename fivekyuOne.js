866dbd2428b993a6efc0d3dc0cc08c77334f7116
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2024-04-01 21:10:24 +0300

    add task

--- fivekyuOne.js
+++ fivekyuOne.js
@@ -16,4 +16,22 @@
 
 //--------------------------------------------------------------
 
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+// function pigIt(str){
+//   return str.split(' ')
+//             .map((word) => {
+//               if (/^[a-zA-Z]+$/.test(word)) {
+//                 return `${word.slice(1)}${word.slice(0, 1)}ay`
+//               } else {
+//                 return word;
+//               }
+//             })
+//             .join(' ')
+// }
 
+// console.log(pigIt('Pig latin is cool')) //'igPay atinlay siay oolcay'
+// console.log(pigIt('This is my string')) //'hisTay siay ymay tringsay'
+// console.log(pigIt('Pig latin is cool')) //'igPay atinlay siay oolcay'
+// console.log(pigIt('Hello world !')) //elloHay orldway !
+
+//--------------------------------------------------------------
